7eef639ee091d35e0ec1dee3b496b2878285e58f
Ankush Aggarwal <ankushagg93@gmail.com>
2015-11-18 22:16:01 -0500

    added blank setting activity

--- app/src/main/java/com/example/anku/sunshine/MainActivity.java
+++ app/src/main/java/com/example/anku/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.anku.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -34,13 +35,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
