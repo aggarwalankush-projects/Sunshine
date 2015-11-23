638b86a6b86a2509ab0d9bb7d85137a8548c001c
Ankush Aggarwal <ankushagg93@gmail.com>
2015-11-23 17:36:31 -0500

    showing details in card view in Tablets

--- app/src/main/java/com/example/anku/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/example/anku/sunshine/DetailActivityFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -19,6 +20,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -147,14 +149,21 @@ public class DetailActivityFragment extends Fragment implements LoaderManager.Lo
                     null
             );
         }
-        getView().setVisibility(View.INVISIBLE);
+        ViewParent vp = getView().getParent();
+        if (vp instanceof CardView) {
+            ((View) vp).setVisibility(View.INVISIBLE);
+        }
+
         return null;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
         if (cursor != null && cursor.moveToFirst()) {
-            getView().setVisibility(View.VISIBLE);
+            ViewParent vp = getView().getParent();
+            if (vp instanceof CardView) {
+                ((View) vp).setVisibility(View.VISIBLE);
+            }
             int weatherId = cursor.getInt(COL_WEATHER_CONDITION_ID);
 
             if (Utility.usingLocalGraphics(getActivity())) {
